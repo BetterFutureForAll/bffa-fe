90e450ecf4bc596cc64c0ff288bf9118e3341716
JOHN NELSON <JohnNelson4850@gmail.com>
2020-08-19 15:31:42 -0700

    data shows in console logs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as d3 from 'd3';
+import * as california_data from './assets/2019-california-results.csv';
 
 function App() {
+  d3.csv(california_data).then(function(data) {
+    console.log(data);
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+
       </header>
     </div>
   );
