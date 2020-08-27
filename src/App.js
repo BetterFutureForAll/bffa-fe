f33bd2c68a2af033c19399c02ada2f3aaea5bcdf
JOHN NELSON <JohnNelson4850@gmail.com>
2020-08-27 14:27:34 -0700

    MapChart

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
-import React from 'react';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import ReactTooltip from "react-tooltip";
 import './App.css';
+import MapChart from './components/MapChart';
 
-import * as d3 from 'd3';
-import * as california_data from './assets/2019-california-results.csv';
-import Tabulate from './components/table';
-import * as dataDefinitions from './assets/definitions.csv';
+// import * as d3 from 'd3';
+// import * as california_data from './assets/2019-california-results.csv';
+// import * as dataDefinitions from './assets/definitions.csv';
+
 
 function App() {
 
-//d3 imports the CSV and is merely logging it
-  d3.csv(california_data).then(function(data) {
-    console.log(data);
-  });
-
-  d3.csv(dataDefinitions).then(function(data) {
-    console.log(data);
-  // //   for (var i = 0; i < data.length; i++) {
-  // //     console.log(data[i].Dimension);
-  // //     console.log(data[i].Component);
-  // });
-  });
-
+  const [content, setContent] = useState("");
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello World
-        </p>
+      <MapChart setTooltipContent={setContent} />
+      <ReactTooltip>{content}</ReactTooltip>
       </header>
-
-    <Tabulate/>
     </div>
 
 
--- src/components/table.js
+++ src/components/table.js
@@ -1,49 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+// import ReactDOM from ReactDOM;
 import * as csvData from '../assets/definitions.csv';
-import * as d3 from 'd3';
+import { csv }from 'd3';
 
 const Tabulate = () => {
-  
-  var tabulate = function (data,columns) {
-    var table = d3.select('body').append('table')
-    var thead = table.append('thead')
-    var tbody = table.append('tbody')
-  
-    thead.append('tr')
-      .selectAll('th')
-        .data(columns)
-        .enter()
-      .append('th')
-        .text(function (d) { return d })
-  
-    var rows = tbody.selectAll('tr')
-        .data(data)
-        .enter()
-      .append('tr')
-  
-    var cells = rows.selectAll('td')
-        .data(function(row) {
-          return columns.map(function (column) {
-            return { column: column, value: row[column] }
-          })
-        })
-        .enter()
-      .append('td')
-        .text(function (d) { return d.value })
-  
-    return table;
-  }
-    
-  d3.csv(csvData,function (data) {
-      var columns = ['Dimension','Component','Indicator name','Definition','Source','Link']
-      tabulate(data,columns)
-  })
 
-  return (
-    <body>
-      {table}
-    </body>
-  );
+  // useEffect(() => { 
+
+  // },[]);
+
+    var tabulate = function (data,columns) {
+      var table = d3.select('body').append('table')
+      var thead = table.append('thead')
+      var tbody = table.append('tbody')
+    
+      thead.append('tr')
+        .selectAll('th')
+          .data(columns)
+          .enter()
+        .append('th')
+          .text(function (d) { return d })
+    
+      var rows = tbody.selectAll('tr')
+          .data(data)
+          .enter()
+        .append('tr')
+    
+      var cells = rows.selectAll('td')
+          .data(function(row) {
+            return columns.map(function (column) {
+              return { column: column, value: row[column] }
+            })
+          })
+          .enter()
+        .append('td')
+          .text(function (d) { return d.value })
+    
+          return table;
+    }
+      
+    d3.csv(csvData,function (data) {
+        var columns = ['Dimension','Component','Indicator name','Definition','Source','Link']
+        tabulate(data,columns)
+    })
+
+    console.log(data);
 };
 
 export default Tabulate;
