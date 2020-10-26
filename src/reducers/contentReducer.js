d7c95f80ddf2aa7d7069a3762f6a8d5f2e648179
JOHN NELSON <JohnNelson4850@gmail.com>
2020-10-26 14:24:45 -0400

    creates Score and Content as blank state

--- src/reducers/contentReducer.js
+++ src/reducers/contentReducer.js
@@ -1,6 +1,8 @@
 import { GET_CONTENT, SET_CONTENT } from '../actions/contentActions';
 
-export default function reducer(state = [], action) {
+const initialState = "";
+
+export default function reducer(state = initialState, action) {
   switch(action.type) {
     case GET_CONTENT:
       return {...action.payload, state };
