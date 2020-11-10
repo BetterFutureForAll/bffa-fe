afd0e6e829ce69f8eb3410159c83ba7cbf94cc20
JOHN NELSON <JohnNelson4850@gmail.com>
2020-11-09 19:35:34 -0800

    viz script

--- src/components/visualization.js
+++ src/components/visualization.js
@@ -1,3 +1,6 @@
+import d3 from 'd3';
+import world from '../assets/world.topojson';
+
 (function() {
   var margin = { top: 50, left: 50, right: 50, bottom: 50 },
     height = 400 - margin.top - margin.bottom,
@@ -10,14 +13,30 @@
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+// import topoJSON and CSV here
+
+  d3.queue()
+    .defer(d3.json, world)
+    .await(ready)
+
+// Projection is created here, round globe to flat monitor.
   var projection = d3.geoMercator()
       .translate([ width / 2, height / 2 ])
+      .scale(100)
 
   var path = d3.geoPath()
     .projection(projection)
 
   function ready (error, data) {
     console.log(data);
+
+    var countries = topojson.feature(data, data.objects.countries).features
+
+    svg.selectAll(".country")
+      .data(countries)
+      .enter().append("path")
+      .attr("class", "country")
+      .attr("d", )
 
   }
 
