1d5b2d443ebf833ffda53110d4fcda8a0bc900a6
JOHN NELSON <johnnelson4850@gmail.com>
2020-12-17 16:02:17 -0800

    proptype validation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Header = ({ selectYears, yearValue, handleSubmit }) => {
   return (
     <form onSubmit={handleSubmit}>
       <label id="years" value={yearValue} >Select a year </label>
-        {selectYears}
+      {selectYears}
     </form>
   );
-}
+};
+
+Header.propTypes = {
+  selectYears: PropTypes.object,
+  yearValue: PropTypes.string,
+  handleSubmit: PropTypes.func
+};
 
 export default Header;
