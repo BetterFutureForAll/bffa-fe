49d83db04477827e256cb0a07fc0a8f687ca554e
John4850 <johnnelson4850@gmail.com>
2021-12-13 16:38:17 -0800

    state all moved to App level

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useRef} from 'react';
+import React, { useEffect, useRef } from 'react';
 // import './Reset.css';
 import './App.css';
 import MapContainer from './containers/Map';
-import { useModal } from './hooks/hooks';
+import { useDataByCountry, useDataByYear, useModal, useToolTip, useYears, useHandleYearChange, useClickedSubCat, useCenter } from './hooks/hooks';
 import ModalDefinitions from './containers/ModalDefinitions';
 import Portal from './containers/Portal';
 import { useWindowSize, useHandleCountryChange, useCountries } from './hooks/hooks';
@@ -14,8 +14,36 @@ function App() {
   let [width, height] = useWindowSize();
   let [countryValue, setCountryValue] = useHandleCountryChange();
   let [countries] = useCountries();
-  
+  let [years] = useYears();
+  let [yearValue, handleYearChange] = useHandleYearChange();
+  let [tooltipContext, setToolTipContext] = useToolTip();
+
   let handleCountryChange = e => setCountryValue(e.target.value);
+
+  // May need Redux to control state.
+  let [spiByYear] = useDataByYear(yearValue);
+  let [spiByCountry, setSpiByCountry] = useDataByCountry(spiByYear, countryValue);
+
+  // let [clickedSubCat, setClickedSubCat] = useClickedSubCat();
+  let svgRef = useRef(null);
+  let [center, setCenter] = useCenter();
+
+
+  let selectYears = (
+    <>
+      <select onChange={handleYearChange} value={yearValue} >
+        {years.map(item => (
+          <option
+            key={item}
+            value={item}
+            onSelect={handleYearChange}
+          >
+            {item}
+          </option>
+        ))}
+      </select>
+    </>
+  );
 
   let selectCountries = (
     <select onChange={handleCountryChange} value={countryValue}>
@@ -28,33 +56,55 @@ function App() {
   );
 
   let children =
-  <> 
-  <ModalDefinitions
-    toggleModal={toggleModal}
-    showModal={showModal}
-    modalRef={modalRef}
-  />
-  </>;
+    <>
+      <ModalDefinitions
+        toggleModal={toggleModal}
+        showModal={showModal}
+        modalRef={modalRef}
+      />
+    </>;
+
+  useEffect(()=>{
+    setCenter([width / 2, height / 2]);
+  }, [height, width]);
+
+  useEffect(() => {
+    setToolTipContext({
+      svgRef, 
+      center, 
+      countryValue,
+      data: spiByCountry
+    });
+    console.log(spiByYear);
+  }, [countryValue, yearValue, spiByCountry])
 
 
   return (
     <div className="App">
       <div id={target} >
-      <Portal
-        id={target}
-        children={showModal? children : null}
-        width={width}
-        height={height}
-      />
-      <MapContainer
-        showModal={showModal}
-        toggleModal={toggleModal}
-        width={width}
-        height={height}
-        selectCountries={selectCountries}
-        countryValue={countryValue}
-        setCountryValue={setCountryValue}
-      />
+        <Portal
+          id={target}
+          children={showModal ? children : null}
+          width={width}
+          height={height}
+        />
+        <MapContainer
+          showModal={showModal}
+          toggleModal={toggleModal}
+          svgRef={svgRef}
+          width={width}
+          height={height}
+          selectYears={selectYears}
+          yearValue={yearValue}
+          selectCountries={selectCountries}
+          countryValue={countryValue}
+          setCountryValue={setCountryValue}
+          tooltipContext={tooltipContext}
+          setToolTipContext={setToolTipContext}
+          center={center}
+          setCenter={setCenter}
+          spiData={spiByYear}
+        />
       </div>
     </div>
   );
