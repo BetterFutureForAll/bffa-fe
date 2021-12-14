399ec53585c34532b7353d70854ae3894cd5eef4
John4850 <johnnelson4850@gmail.com>
2021-12-13 16:39:22 -0800

    single petal

--- src/components/ToolTip.js
+++ src/components/ToolTip.js
@@ -9,35 +9,95 @@ import {
 
 // needs X, Y, and SPI Data set
 const ToolTip = (tooltipContext) => {
-  // let graphicTooltip = <foreignobject x={x} y={y} width={100} height={50}/>;
-  // let textToolTip = <foreignobject x={x} y={y} width={100} height={50}/>;
+
   let spiScale = d3.scaleLinear().domain([0, 100]).range([0, 100]);
-  
+  let petalPath = 'M 0 0 c 100 100 80 0 100 0 C 80 0 100 -100 0 0';
+  let subPetalPath = "M 0 0 L 85 15 A 1 1 0 0 0 85 -15 L 0 0";
+
   function ready() {
-    let {svgRef, center, name } = tooltipContext.context;
-    console.log('ToolTip function', tooltipContext);
-    console.log(svgRef);
-    if(!svgRef) return;
+    let { svgRef, center, name, data } = tooltipContext.context;
+
+    if (!data) return;
+
     let x = center[0];
     let y = center[1];
-    let svg = d3.select(svgRef.current);
-    console.log('svg',svg);
-    svg.insert("circle")
-    .attr('class', 'outer')
-    .attr('id', name)
-    .attr("cx", x)
-    .attr("cy", y)
-    .attr("r", 50)
-    .transition().duration([750])
-    .attr("r", spiScale(100))
-    .style('fill', '#c4c2c4')
-    .style("opacity", "0.5")
-    .style('stroke', 'black')
 
+    let svg = d3.select(svgRef.current);
+    svg.selectAll('.graphicTooltip').remove();
+    let toolTip = svg.insert("g")
+      .attr('class', 'graphicTooltip')
+    // let toolTip = svg.selectAll('.graphicTooltip');
+    toolTip
+      .selectAll('.outer')
+      .data([data[0]])
+      .join('circle')
+      .attr('class', 'outer')
+      .attr('id', d=>{
+        return d})
+      .attr("cx", x)
+      .attr("cy", y)
+      // .attr("r", 0)
+      // .transition().duration([750])
+      .attr("r", 100)
+      .style('fill', '#c4c2c4')
+      .style("opacity", "0.5")
+      .style('stroke', 'black')
+
+    toolTip
+      .selectAll('.inner')
+      .data([data[0]])
+      .join("circle")
+      .attr('class', 'inner')
+      .attr('id', name)
+      .attr("cx", center[0])
+      .attr("cy", center[1])
+      .attr("r", 0)
+      .transition().duration([750])
+      .attr("r", d => d ? +d["Social Progress Index"] : 0  )
+      .style('fill', d => colorScale(d ? +d["Social Progress Index"] : 0 ))
+      .style('stroke', 'black')
+      .attr("cursor", "pointer")
+// make 3 separate petals here
+      toolTip
+        .selectAll('.petalBackgroundPath')
+        .data([data[0]])
+        .join('path')
+        .attr('class', 'petalBackgroundPath')
+        .attr("id", (d, i) => `${d["Country Name"]}`)
+        .attr('d', petalPath)
+        .attr('transform', `translate(${center[0]}, ${center[1]}) rotate(${30}) scale(${0})`)
+        .transition().duration(750)
+        .attr('transform', `translate(${center[0]}, ${center[1]}) rotate(${30}) scale(${spiScale(100) *.01})`)
+        .style('stroke', 'black')
+        .style('fill', d => colorScale(100))
+        .style("opacity", "0.50")
+        .attr("cursor", "pointer")
+
+      toolTip
+        .selectAll('.petalPath')
+        .data([data[0]])
+        .join('path')
+        .attr('class', 'petalPath')
+        .attr("id", (d, i) => `${d.id}${d.text}`)
+        .attr('d', petalPath)
+        .attr('transform', `translate(${center[0]}, ${center[1]}) rotate(${30}) scale(${0})`)
+        .style('fill', colorScale(0))
+        .transition().duration(1000)
+        .attr('transform', d => {
+          return `translate(${center[0]}, ${center[1]}) rotate(${30}) scale(${spiScale(d ? +d["Basic Human Needs"] : 0) *.01})`
+        })
+        .style('stroke', 'black')
+        .style('fill', d => colorScale(d ? +d["Basic Human Needs"] : 0))
+        .attr("cursor", "pointer")
+
+
+
+    toolTip
+      .on("mouseleave", () => toolTip.remove())
   };
 
 
-  useEffect(()=>{
+  useEffect(() => {
     ready();
   }, [tooltipContext]);
 
