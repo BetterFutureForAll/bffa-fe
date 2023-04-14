b61b94434bd80c9c8652d5bbf330fe50a054ee2b
John4850 <johnnelson4850@gmail.com>
2023-04-13 17:15:47 -0700

    fixed sizing and zooming issues

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -8,7 +8,7 @@ const Legend = ({ width, height }) => {
   let legendRef = useRef(null);
   let legendData = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
-  let squareSize = ((width * .75) / 12 > 25) ? (width * .75) / 12 : 26;
+  let squareSize = ((width) / 13 > 25) ? (width) / 13 : 26;
 
   if (width < 376) {
     squareSize = 26;
--- src/components/MapMaker.js
+++ src/components/MapMaker.js
@@ -20,10 +20,9 @@ const MapMaker = ({ mapProps, setCountryValue, countryValue }) => {
     if (mapData && spiData && !loading) {
 
       function ready(data) {
-        let checkedSize = Math.min(width, height);
         let projection = d3.geoEqualEarth()
-          .scale(checkedSize / 5)
-          .translate([width / 2, height / 2])
+          .scale(width/5)
+          .translate([width / 2 - 25, height / 2])
 
         let path = d3.geoPath().projection(projection);
         let mapFeatures = feature(data[0], data[0].objects.countries).features;
@@ -36,10 +35,10 @@ const MapMaker = ({ mapProps, setCountryValue, countryValue }) => {
         }
 
         function countryMouseOver(event, d) {
-          if(!d.properties) return
-            let spiMatch = getSpiData(d);
-            let mapName = spiMatch ? spiMatch[0].country : "World";
-            setCountryValue(mapName);
+          if (!d.properties) return
+          let spiMatch = getSpiData(d);
+          let mapName = spiMatch ? spiMatch[0].country : "World";
+          setCountryValue(mapName);
         };
 
         let zoomed = (event, d) => {
@@ -51,18 +50,20 @@ const MapMaker = ({ mapProps, setCountryValue, countryValue }) => {
         };
 
         const zoom = d3.zoom()
-          .translateExtent([[-.15 * width, -.15 * height], [width * 1.5, height * 1.25]])
+          .translateExtent([[-.15 * width, -.15 * height], [width * 1.25, height * 1.25]])
           .scaleExtent([1, 10])
           .on('zoom', zoomed)
 
         function zoomToTarget(value) {
           let target = spiNameGroup.get(value)
           let targetId = `#${target[0].spicountrycode}_target`;
-          if(!target || svg.select(targetId).empty()) return zoomToCenter();
-          let bounds = svg.select(targetId).node().getBBox();
+
+          if (!target || svg.select(targetId).empty()) return zoomToCenter(); //catches clicks on borders
+
+          let bounds = svg.select(targetId).node().getBoundingClientRect();
           const cx = bounds.x + bounds.width / 2;
           const cy = bounds.y + bounds.height / 2;
-          const scale = 3;
+          const scale = 5;
           const transform = d3.zoomIdentity.translate(width / 2 - scale * cx, height / 2 - scale * cy).scale(scale);
           svg.transition().duration(500).call(zoom.transform, transform);
         }
@@ -74,22 +75,23 @@ const MapMaker = ({ mapProps, setCountryValue, countryValue }) => {
 
         // *** Top Level Selector (ViewBox) ***
         let svg = d3.select(svgRef.current)
-          .attr("id", "viewbox")
+          .attr("id", "viewBox")
           .attr("viewBox", [0, 0, width, height])
-          .attr('preserveAspectRatio', 'xMinYMid')
+          .attr('preserveAspectRatio', 'xMinYMid meet')
           .on('zoom', zoom)
-        
+
         //White rectangle to allow clicks outside of countries to zoomToCenter
         svg.selectAll('.background')
-        .data([1]).join('rect')
-        .attr('class', 'background')
-        .attr("width", width).attr("height", height)
-        .attr("fill", "white")
-        .on('click', zoomToCenter)
+          .data([1]).join('rect')
+          .attr("id", "background")
+          .attr('class', 'background')
+          .attr("width", width).attr("height", height)
+          .attr("fill", "white")
+          .on('click', zoomToCenter)
 
         svg.call(zoom);
         svg.selectAll('.countries').remove();
-        
+
         // *** Country groupings ***
         let g = svg.selectAll('.countries').data([1]).join("g").attr('class', 'countries');
         let countries = g.selectAll(".country")
@@ -117,8 +119,9 @@ const MapMaker = ({ mapProps, setCountryValue, countryValue }) => {
 
         countries.exit().remove();
 
+        //ZoomToCenter and Tooltip Center targets
         g.selectAll(`.toolTipTarget`)
-          //** Filter Country matches here  */
+          //** Filter so we only zoom to valid countries  */
           .data(mapFeatures.filter(d => d.properties.mapId !== "ATA"))
           .join('circle')
           .attr('class', 'toolTipTarget')
@@ -129,7 +132,8 @@ const MapMaker = ({ mapProps, setCountryValue, countryValue }) => {
           })
           .attr("cx", d => path.centroid(d)[0])
           .attr("cy", d => path.centroid(d)[1])
-          .attr("r", 0)
+          .attr("r", 1) //Safari requires an radius to have a value or BBox fails
+          .attr("fill", "none") //Hide the Tooltip/Zoom target
 
         //append a centerTarget for the world.
         g.selectAll(`.toolTipTarget`)
@@ -141,6 +145,7 @@ const MapMaker = ({ mapProps, setCountryValue, countryValue }) => {
 
         // // *** borders / whitespace mesh ***
         let borders = g.append("path")
+          .attr("id", (d, i) => `border${i}`)
           .attr("class", "border")
           .attr("fill", "none")
           .attr("stroke", "white")
@@ -150,7 +155,7 @@ const MapMaker = ({ mapProps, setCountryValue, countryValue }) => {
         borders.exit().remove();
 
         //Programmatically call zoom after rerender due to values possibly changing from dropdown.
-        if(countryValue === "World") zoomToCenter();
+        if (countryValue === "World") zoomToCenter();
         else zoomToTarget(countryValue);
 
         //if that map renders after the tooltip, put the tooltip back on top. 
